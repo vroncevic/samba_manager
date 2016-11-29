ed4f48b716ac0bbe1b71b7e9b1cdf76f17200a1d
Vladimir Roncevic <elektron.ronca@gmail.com>
2016-11-29 18:18:53 +0100

    Update logging, usage, progressbar, add .editorconfig, README.md

--- bin/smbmanager.sh
+++ bin/smbmanager.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# @brief   Samba Server Manager
+# @brief   Samba Server Manager (wrapper)
 # @version ver.1.0
 # @date    Mon Jun 02 13:36:32 2015
 # @company Frobas IT Department, www.frobas.com 2015
@@ -11,43 +11,46 @@ UTIL_VERSION=ver.1.0
 UTIL=$UTIL_ROOT/sh-util-srv/$UTIL_VERSION
 UTIL_LOG=$UTIL/log
 
+. $UTIL/bin/devel.sh
+. $UTIL/bin/usage.sh
 . $UTIL/bin/checkroot.sh
 . $UTIL/bin/checktool.sh
 . $UTIL/bin/checkop.sh
 . $UTIL/bin/logging.sh
-. $UTIL/bin/usage.sh
-. $UTIL/bin/devel.sh
+. $UTIL/bin/sendmail.sh
+. $UTIL/bin/loadconf.sh
+. $UTIL/bin/loadutilconf.sh
+. $UTIL/bin/progressbar.sh
 
 SMBMANAGER_TOOL=smbmanager
 SMBMANAGER_VERSION=ver.1.0
 SMBMANAGER_HOME=$UTIL_ROOT/$SMBMANAGER_TOOL/$SMBMANAGER_VERSION
 SMBMANAGER_CFG=$SMBMANAGER_HOME/conf/$SMBMANAGER_TOOL.cfg
+SMBMANAGER_UTIL_CFG=$SMBMANAGER_HOME/conf/${SMBMANAGER_TOOL}_util.cfg
 SMBMANAGER_LOG=$SMBMANAGER_HOME/log
 
 declare -A SMBMANAGER_USAGE=(
-	[TOOL_NAME]="__$SMBMANAGER_TOOL"
-	[ARG1]="[PROCESS]   smb | nmb | winbind | all"
-	[ARG2]="[OPERATION] start | stop | restart | status | version"
-	[EX-PRE]="# Restart smb service"
-	[EX]="__$SMBMANAGER_TOOL smb restart"
+	[USAGE_TOOL]="__$SMBMANAGER_TOOL"
+	[USAGE_ARG1]="[PROCESS]   smb | nmb | winbind | all"
+	[USAGE_ARG2]="[OPERATION] start | stop | restart | status | version"
+	[USAGE_EX_PRE]="# Restart smb service"
+	[USAGE_EX]="__$SMBMANAGER_TOOL smb restart"
 )
 
-declare -A LOG=(
-	[TOOL]="$SMBMANAGER_TOOL"
-	[FLAG]="info"
-	[PATH]="$SMBMANAGER_LOG"
-	[MSG]=""
+declare -A SMBMANAGER_LOG=(
+	[LOG_TOOL]="$SMBMANAGER_TOOL"
+	[LOG_FLAG]="info"
+	[LOG_PATH]="$SMBMANAGER_LOG"
+	[LOG_MSGE]="None"
+)
+
+declare -A PB_STRUCTURE=(
+	[BAR_WIDTH]=50
+	[MAX_PERCENT]=100
+	[SLEEP]=0.01
 )
 
 TOOL_DBG="false"
-
-SYSTEMCTL_PATH="/usr/bin/systemctl"
-PDBEDIT="/usr/bin/pdbedit"
-SMBSTATUS="/usr/bin/smbstatus"
-SMBD="/usr/sbin/smbd"
-NMBD="/usr/sbin/nmbd"
-WINBINDD="/usr/sbin/winbindd"
-SMB_OP_LIST=( start stop restart status version )
 
 #
 # @brief  Get samba version
@@ -59,7 +62,7 @@ SMB_OP_LIST=( start stop restart status version )
 # __smbversion 
 # local STATUS=$?
 #
-# if [ "$STATUS" -eq "$SUCCESS" ]; then
+# if [ $STATUS -eq $SUCCESS ]; then
 #	# true
 #	# notify admin | user
 # else
@@ -71,16 +74,25 @@ SMB_OP_LIST=( start stop restart status version )
 #
 function __smbversion() {
 	local FUNC=${FUNCNAME[0]}
-	local MSG=""
-	__checktool "$SMBD"
+	local MSG="None"
+	__checktool "${configsmbmanagerutil[SMBD]}"
 	local STATUS=$?
-	if [ "$STATUS" -eq "$SUCCESS" ]; then
+	if [ $STATUS -eq $SUCCESS ]; then
 		if [ "$TOOL_DBG" == "true" ]; then
 			MSG="Version of samba server"
 			printf "$DSTA" "$SMBMANAGER_TOOL" "$FUNC" "$MSG"
 		fi
-		eval "$SMBD -V"
+		eval "${configsmbmanagerutil[SMBD]} -V"
 		return $SUCCESS
+	fi
+	MSG="Missing external tool ${configsmbmanagerutil[SMBD]}"
+	if [ "${configsmbmanager[LOGGING]}" == "true" ]; then
+		OSSL_LOGGING[LOG_MSGE]="$MSG"
+		OSSL_LOGGING[LOG_FLAG]="error"
+		__logging OSSL_LOGGING
+	fi
+	if [ "${configsmbmanager[EMAILING]}" == "true" ]; then
+		__sendmail "$MSG" "${configsmbmanager[ADMIN_EMAIL]}"
 	fi
 	return $NOT_SUCCESS
 }
@@ -108,16 +120,25 @@ function __smbversion() {
 function __smbinfo() {
 	local FUNC=${FUNCNAME[0]}
 	local MSG=""
-    __checktool "$SMBSTATUS"
+    __checktool "${configsmbmanagerutil[SMBSTATUS]}"
     local STATUS=$?
-    if [ "$STATUS" -eq "$SUCCESS" ]; then
+    if [ $STATUS -eq $SUCCESS ]; then
 		if [ "$TOOL_DBG" == "true" ]; then
 			MSG="Get samba info"
 			printf "$DSTA" "$SMBMANAGER_TOOL" "$FUNC" "$MSG"
 		fi
-        eval "$SMBSTATUS -v"
+        eval "${configsmbmanagerutil[SMBSTATUS]} -v"
         return $SUCCESS
-    fi 
+    fi
+	MSG="Missing external tool ${configsmbmanagerutil[SMBSTATUS]}"
+	if [ "${configsmbmanager[LOGGING]}" == "true" ]; then
+		OSSL_LOGGING[LOG_MSGE]="$MSG"
+		OSSL_LOGGING[LOG_FLAG]="error"
+		__logging OSSL_LOGGING
+	fi
+	if [ "${configsmbmanager[EMAILING]}" == "true" ]; then
+		__sendmail "$MSG" "${configsmbmanager[ADMIN_EMAIL]}"
+	fi
 	return $NOT_SUCCESS
 }
 
@@ -144,16 +165,25 @@ function __smbinfo() {
 function __smblist() {
 	local FUNC=${FUNCNAME[0]}
 	local MSG=""
-    __checktool "$PDBEDIT"
+    __checktool "${configsmbmanagerutil[PDBEDIT]}"
     local STATUS=$?
-    if [ "$STATUS" -eq "$SUCCESS" ]; then
+    if [ $STATUS -eq $SUCCESS ]; then
 		if [ "$TOOL_DBG" == "true" ]; then
 			MSG="List samba"
 			printf "$DSTA" "$SMBMANAGER_TOOL" "$FUNC" "$MSG"
 		fi
-        eval "$PDBEDIT -L -v"
+        eval "${configsmbmanagerutil[PDBEDIT]} -L -v"
         return $SUCCESS
     fi 
+	MSG="Missing external tool ${configsmbmanagerutil[PDBEDIT]}"
+	if [ "${configsmbmanager[LOGGING]}" == "true" ]; then
+		OSSL_LOGGING[LOG_MSGE]="$MSG"
+		OSSL_LOGGING[LOG_FLAG]="error"
+		__logging OSSL_LOGGING
+	fi
+	if [ "${configsmbmanager[EMAILING]}" == "true" ]; then
+		__sendmail "$MSG" "${configsmbmanager[ADMIN_EMAIL]}"
+	fi
 	return $NOT_SUCCESS
 }
 
@@ -183,15 +213,24 @@ function __smboperation() {
     if [ -n "$OPERATION" ]; then
 		local FUNC=${FUNCNAME[0]}
 		local MSG=""
-		__checktool "$SYSTEMCTL_PATH"
+		__checktool "${configsmbmanagerutil[SYSTEMCTL]}"
     	local STATUS=$?
-		if [ "$STATUS" -eq "$SUCCESS" ]; then
+		if [ $STATUS -eq $SUCCESS ]; then
 			if [ "$TOOL_DBG" == "true" ]; then
 				MSG="smb service [$OPERATION]"
 				printf "$DSTA" "$SMBMANAGER_TOOL" "$FUNC" "$MSG"
 			fi
-		    eval "$SYSTEMCTL_PATH $OPERATION smb.service"
+		    eval "${configsmbmanagerutil[SYSTEMCTL]} $OPERATION smb.service"
 		    return $SUCCESS 
+		fi
+		MSG="Missing external tool ${configsmbmanagerutil[SYSTEMCTL]}"
+		if [ "${configsmbmanager[LOGGING]}" == "true" ]; then
+			OSSL_LOGGING[LOG_MSGE]="$MSG"
+			OSSL_LOGGING[LOG_FLAG]="error"
+			__logging OSSL_LOGGING
+		fi
+		if [ "${configsmbmanager[EMAILING]}" == "true" ]; then
+			__sendmail "$MSG" "${configsmbmanager[ADMIN_EMAIL]}"
 		fi
 		return $NOT_SUCCESS
     fi 
@@ -223,16 +262,25 @@ function __nmboperation() {
     local OPERATION=$1
     if [ -n "$OPERATION" ]; then
 		local FUNC=${FUNCNAME[0]}
-		local MSG=""
-		__checktool "$SYSTEMCTL_PATH"
+		local MSG="None"
+		__checktool "${configsmbmanagerutil[SYSTEMCTL]}"
     	local STATUS=$?
-		if [ "$STATUS" -eq "$SUCCESS" ]; then
+		if [ $STATUS -eq $SUCCESS ]; then
 			if [ "$TOOL_DBG" == "true" ]; then
 				MSG="nmb service [$OPERATION]"
 				printf "$DSTA" "$SMBMANAGER_TOOL" "$FUNC" "$MSG"
 			fi
-		    eval "$SYSTEMCTL_PATH $OPERATION nmb.service"
+		    eval "${configsmbmanagerutil[SYSTEMCTL]} $OPERATION nmb.service"
 		    return $SUCCESS 
+		fi
+		MSG="Missing external tool ${configsmbmanagerutil[SYSTEMCTL]}"
+		if [ "${configsmbmanager[LOGGING]}" == "true" ]; then
+			OSSL_LOGGING[LOG_MSGE]="$MSG"
+			OSSL_LOGGING[LOG_FLAG]="error"
+			__logging OSSL_LOGGING
+		fi
+		if [ "${configsmbmanager[EMAILING]}" == "true" ]; then
+			__sendmail "$MSG" "${configsmbmanager[ADMIN_EMAIL]}"
 		fi
 		return $NOT_SUCCESS
     fi 
@@ -264,16 +312,25 @@ function __winbindoperation() {
     local OPERATION=$1
     if [ -n "$OPERATION" ]; then
 		local FUNC=${FUNCNAME[0]}
-		local MSG=""
-		__checktool "$SYSTEMCTL_PATH"
+		local MSG="None"
+		__checktool "${configsmbmanagerutil[SYSTEMCTL]}"
     	local STATUS=$?
-		if [ "$STATUS" -eq "$SUCCESS" ]; then
+		if [ $STATUS -eq $SUCCESS ]; then
 			if [ "$TOOL_DBG" == "true" ]; then
 				MSG="winbind service [$OPERATION]"
 				printf "$DSTA" "$SMBMANAGER_TOOL" "$FUNC" "$MSG"
 			fi
-		    eval "$SYSTEMCTL_PATH $OPERATION winbind.service"
+		    eval "${configsmbmanagerutil[SYSTEMCTL]} $OPERATION winbind.service"
 		    return $SUCCESS 
+		fi
+		MSG="Missing external tool ${configsmbmanagerutil[SYSTEMCTL]}"
+		if [ "${configsmbmanager[LOGGING]}" == "true" ]; then
+			OSSL_LOGGING[LOG_MSGE]="$MSG"
+			OSSL_LOGGING[LOG_FLAG]="error"
+			__logging OSSL_LOGGING
+		fi
+		if [ "${configsmbmanager[EMAILING]}" == "true" ]; then
+			__sendmail "$MSG" "${configsmbmanager[ADMIN_EMAIL]}"
 		fi
 		return $NOT_SUCCESS
     fi 
@@ -298,14 +355,42 @@ function __smbmanager() {
     local OPERATION=$2
     if [ -n "$PROCESS" ] && [ -n "$OPERATION" ]; then
 		local FUNC=${FUNCNAME[0]}
-		local MSG=""
+		local MSG="Loading basic and util configuration"
+		printf "$SEND" "$SMBMANAGER_TOOL" "$MSG"
+		__progressbar PB_STRUCTURE
+		printf "%s\n\n" ""
+		declare -A configsmbmanager=()
+		__loadconf $SMBMANAGER_CFG configsmbmanager
+		local STATUS=$?
+		if [ $STATUS -eq $NOT_SUCCESS ]; then
+			MSG="Failed to load tool script configuration"
+			if [ "$TOOL_DBG" == "true" ]; then
+				printf "$DSTA" "$SMBMANAGER_TOOL" "$FUNC" "$MSG"
+			else
+				printf "$SEND" "$SMBMANAGER_TOOL" "$MSG"
+			fi
+			exit 129
+		fi
+		declare -A configsmbmanagerutil=()
+		__loadutilconf $SMBMANAGER_UTIL_CFG configsmbmanagerutil
+		STATUS=$?
+		if [ $STATUS -eq $NOT_SUCCESS ]; then
+			MSG="Failed to load tool script utilities configuration"
+			if [ "$TOOL_DBG" == "true" ]; then
+				printf "$DSTA" "$SMBMANAGER_TOOL" "$FUNC" "$MSG"
+			else
+				printf "$SEND" "$SMBMANAGER_TOOL" "$MSG"
+			fi
+			exit 130
+		fi
 		if [ "$TOOL_DBG" == "true" ]; then
 			MSG="Samba Server Manager"
 			printf "$DSTA" "$SMBMANAGER_TOOL" "$FUNC" "$MSG"
 		fi
+		SMB_OP_LIST=( start stop restart status version )
         __checkop "$OPERATION" "${SMB_OP_LIST[*]}"
         local STATUS=$?
-        if [ "$STATUS" -eq "$SUCCESS" ]; then
+        if [ $STATUS -eq $SUCCESS ]; then
             case "$PROCESS" in
                 "smb")
 		                    __smboperation $OPERATION
@@ -335,6 +420,12 @@ function __smbmanager() {
 							__smbversion
 							;;
             esac
+			MSG="Operation: $OPERATION with service(s): $PROCESS done"
+			if [ "${configsmbmanager[LOGGING]}" == "true" ]; then
+				OSSL_LOGGING[LOG_MSGE]="$MSG"
+				OSSL_LOGGING[LOG_FLAG]="info"
+				__logging OSSL_LOGGING
+			fi
 			if [ "$TOOL_DBG" == "true" ]; then
 				printf "$DEND" "$SMBMANAGER_TOOL" "$FUNC" "Done"
 			fi
@@ -342,7 +433,7 @@ function __smbmanager() {
         fi
 		exit 129
     fi 
-	__usage $SMBMANAGER_USAGE
+	__usage SMBMANAGER_USAGE
 	exit 128
 }
 
@@ -363,3 +454,4 @@ if [ "$STATUS" -eq "$SUCCESS" ]; then
 fi
 
 exit 127
+
